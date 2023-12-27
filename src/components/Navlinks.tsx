e52e1fab471ceb5ff020a95868ac31a9acb3a5be
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2023-12-28 00:02:05 +0100

    feat(dashboard): add nav links

--- src/components/Navlinks.tsx
+++ src/components/Navlinks.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
+const links = [
+  { id: 1, label: "Chat", href: "/chat" },
+  { id: 2, label: "Tours", href: "/tours" },
+  { id: 3, label: "New tour", href: "/tours/new-tour" },
+  { id: 4, label: "Profile", href: "/profile" },
+];
+
 const Navlinks = () => {
-  return <div>Navlinks</div>;
+  return (
+    <div className='menu text-base-content'>
+      {links.map((link) => {
+        return (
+          <li key={link.href}>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Navlinks;
