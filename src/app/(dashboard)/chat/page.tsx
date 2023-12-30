936593c72edd9f1a4fcd821a731c57adb2338fd2
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2023-12-30 02:03:33 +0100

    feat(components): add chat component

--- src/app/(dashboard)/chat/page.tsx
+++ src/app/(dashboard)/chat/page.tsx
@@ -1,7 +1,20 @@
+import Chat from "@/components/Chat";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import React from "react";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const queryClient = new QueryClient();
+  return (
+    <>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Chat />
+      </HydrationBoundary>
+    </>
+  );
 };
 
 export default ChatPage;
---
+++ src/components/Chat.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { generateChatResponse } from "@/utils/action";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+
+const Chat = () => {
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useMutation({
+    mutationFn: generateChatResponse,
+  });
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(text);
+  };
+  return (
+    <div className='min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]'>
+      <div>
+        <h2 className='text-5xl'>messages</h2>
+      </div>
+      <form onSubmit={handleSubmit} className='max-w-4xl pt-12'>
+        <div className='join w-full'>
+          <input
+            type='text'
+            placeholder='Message GeniusGPT'
+            className='input input-bordered join-item w-full'
+            value={text}
+            required
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button type='submit' className='btn btn-primary join-item'>
+            Ask question
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Chat;
