96eeff516bc6d53ba927de8b0040ae5ea68bee25
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2025-01-22 00:28:58 +0100

    refactor(Providers): update type for children prop and enhance query client configuration

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,17 +5,20 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React, { useState } from "react";
 import { Toaster } from "react-hot-toast";
 
-const Providers = ({ children }: { children: any }) => {
-  const [queryClient] = useState(() => {
-    return new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: 1000 * 60 * 5,
-          refetchOnWindowFocus: false,
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5, // 5 minutes
+            gcTime: 1000 * 60 * 30, // 30 minutes
+            retry: 3,
+            refetchOnWindowFocus: false,
+          },
         },
-      },
-    });
-  });
+      }),
+  );
   return (
     <>
       <QueryClientProvider client={queryClient}>
